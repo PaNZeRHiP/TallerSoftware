0ecd888677b8dd4d964c5f183b819bf91662a4c8
JhonatanAl <jhonatan.alvarez@alumnos.ucn.cl>
2021-01-15 11:19:39 -0300

    figure commit

---
+++ TallerSoftware/src/tallersoftware/Figure.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallersoftware;
+
+/**
+ *
+ * @author SrFib
+ */
+public class Figure {
+    protected String name;
+    
+    public Figure(String name){
+        this.name=name;
+    }
+    
+}
